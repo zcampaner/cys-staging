ad3a97558a0595f8db61a5a799c404515b39f42a
zcampaner <zcampaner@gmail.com>
2016-04-21 14:38:40 +0800

    edited logout-php

--- logout.php
+++ logout.php
@@ -1,5 +1,6 @@
 <?php
 session_start();
 session_destroy();
-header("location : login.php");
+echo "<script language='javascript' type='text/javascript'>alert('Successfully Logged Logout!')</script>";
+echo "<script language='javascript' type='text/javascript'>window.open('index.php','_self')</script>";
 
