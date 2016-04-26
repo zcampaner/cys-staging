80b4bd81a5622ad08756d218c377d4d910ccf9af
garciaivanj31 <garciaivanj31@gmail.com>
2016-04-26 16:51:51 +0800

    hotel_chains.ph etc updated record

--- edit_file.php
+++ edit_file.php
@@ -1,15 +1,28 @@
 <?php
 include("dbconnection.php");
 
-	if(isset($_POST['login_name'])){
-		$query = "UPDATE system.users SET password = :password, first_name = :first_name, last_name = :last_name, email = :email WHERE login_name = :login_name";
+    if(isset($_POST['login_name'])){
+        $query = "UPDATE system.users SET password = :password, first_name = :first_name, last_name = :last_name, email = :email WHERE login_name = :login_name";
+
+        $stmt = $db->prepare($query); 
+        $stmt->bindParam(':login_name', $_POST['login_name']);
+        $stmt->bindParam(':password', md5($_POST['password']));
+        $stmt->bindParam(':first_name', $_POST['first_name']);
+        $stmt->bindParam(':last_name', $_POST['last_name']);
+        $stmt->bindParam(':email', $_POST['email']);
+
+            $stmt->execute();
+
+        echo $stmt->rowCount();
+    }
+    
+	if(isset($_POST['chain_code'])){
+		$query = "UPDATE system.hotel_chains SET chain = :chain, description = :description WHERE chain_code = :chain_code";
 
 		$stmt = $db->prepare($query); 
-		$stmt->bindParam(':login_name', $_POST['login_name']);
-		$stmt->bindParam(':password', md5($_POST['password']));
-		$stmt->bindParam(':first_name', $_POST['first_name']);
-		$stmt->bindParam(':last_name', $_POST['last_name']);
-		$stmt->bindParam(':email', $_POST['email']);
+		$stmt->bindParam(':chain_code', $_POST['chain_code']);
+		$stmt->bindParam(':chain', $_POST['chain']);
+		$stmt->bindParam(':description', $_POST['description']);
 
             $stmt->execute();
 
--- template_edit.php
+++ template_edit.php
@@ -6,6 +6,18 @@ include("dbconnection.php");
     $ppid = $_POST['login_name'];
 
     $sqladd="SELECT login_name,first_name,last_name,email FROM system.users WHERE login_name='$ppid' limit 1";
+    $res=$db->prepare($sqladd);
+    $res->execute();
+    $result = $res->fetchALL(PDO::FETCH_ASSOC); 
+
+    echo json_encode($result);
+}
+
+    if(isset($_POST['chain_code'])){
+
+    $ppid = $_POST['chain_code'];
+
+    $sqladd="SELECT chain_code,chain,description FROM system.hotel_chains WHERE chain_code='$ppid' limit 1";
 	$res=$db->prepare($sqladd);
 	$res->execute();
 	$result = $res->fetchALL(PDO::FETCH_ASSOC);	
