0c6af0f57c41ffa81dbb304590f1d7d316c730c5
zcampaner <zcampaner@gmail.com>
2016-04-27 10:24:40 +0800

    fixed issue on committing js files

---
+++ assets/js/loading.js
@@ -0,0 +1,7 @@
+
+//paste this code under head tag or in a seperate js file.
+	// Wait for window load
+	$(window).load(function() {
+		// Animate loader off screen
+		$(".se-pre-con").fadeOut("slow");;
+	});
