d11c00b299443940a21b9e0c4585d060400f72d1
garciaivanj31 <garciaivanj31@gmail.com>
2016-05-16 14:36:51 +0800

    updated klipfolio.php

--- klipfolio.php
+++ klipfolio.php
@@ -1,12 +1,11 @@
 <?php
 
- // header('Content-type: text/xml');
-date_default_timezone_set('Asia/Manila');
-
+header('Content-type: text/xml');
+//date_default_timezone_set('Asia/Manila');
+date_default_timezone_set('UTC');
 try {
- error_reporting(E_ALL);
-
-  	$dbname = 'dev_dwhdb';
+ 
+	$dbname = 'dev_dwhdb';
   	$host = '10.1.2.222';
   	$user = 'guest';
   	$pass = 'pass';
@@ -17,8 +16,7 @@ try {
   	$endDate = isset($_GET['end_date']) ? $_GET['end_date'] :(string) date('Y-m-d');
 
   	$db = new PDO("$driver:dbname=$dbname;host=$host;user=$user;password=$pass;");
- 	// var_export($db);die;
-	$stmt = $db->prepare(
+  	$stmt = $db->prepare(
 		"SELECT 
 			t1.id,
 			t1.first_name, 
@@ -37,12 +35,15 @@ try {
 			t1.no_of_nights,
 			t1.total_no_of_rooms,
 			t2.base_rate,
+			t2.rate_plan_id,
+			t2.room_id,
+			t2.reservation_date,
 			AVG(t2.base_rate_total) OVER (PARTITION BY t1.id ORDER BY t1.id DESC) AS average_room_rate 
 	 	FROM gi.t_reservations t1
 	  	INNER JOIN gi.m_status_type t3 ON t1.status_id = t3.id 
 	  	INNER JOIN gi.t_reservation_details t2 ON t1.id = t2.reservation_id 
 	  	WHERE t1.created_date::DATE BETWEEN :start_date AND :end_date
-	  	ORDER BY t1.created_date DESC, t1.id DESC, t2.reservation_id DESC"
+	  	ORDER BY t1.created_date DESC, t1.id DESC, t2.reservation_id, t2.reservation_date, t2.rate_plan_id, t2.room_id DESC"
 	);
 
 	$stmt->bindParam(':start_date', $startDate);
@@ -58,25 +59,30 @@ try {
 		foreach($result as $key => $row) {
 
 		  	if ($row['id'] != $reservation_id) {
+				
 				$reservation_id = $row['id'];
 			  
 				$reservation = $xmlObj->addChild('Reservation');
-				
+				$reservation->addAttribute('ID',$row['id']);
 				$check_in_date = date_create_from_format('Y-m-d', $row['check_in_date']);
 				$check_out_date = date_create_from_format('Y-m-d', $row['check_out_date']);
 				$reservation_date = new DateTime($row['created_date']);
 				$average_booking_window = $check_in_date->diff($reservation_date)->days;
-
+				
 				$los = $check_out_date->diff($check_in_date)->days;
 				
 				$reservation->addChild('LengthOfStay', $row['no_of_nights']);
 				$name = $row['first_name'].' '.$row['last_name'];
-				$ratePlan = $reservation->addChild('RatePlan', $row['rate_plan_name']);
-				$roomTypes = $reservation->addChild('RoomTypes');
-				$Roomtype = $roomTypes->addChild('RoomType', $row['room_name']);
-				$Roomtype->addAttribute('ID', $row['id']);
+				$reservation->addChild('RatePlans');
+				
+				$ratePlan = $reservation->RatePlans->addChild('RatePlan', $row['rate_plan_name']);
+				$ratePlan->addAttribute('ID',$row['rate_plan_id']);
+				$ratePlan->addAttribute('totalRoomBaseRate', $row['base_rate']);
+				$Roomtype = $ratePlan->addChild('RoomType', $row['room_name']);
+				$Roomtype->addAttribute('ID', $row['room_id']);
 				$Roomtype->addAttribute('NoOfRooms', $row['no_of_rooms']);
 				$Roomtype->addAttribute('BaseRate', $row['base_rate']);
+				$Roomtype->addAttribute('Date',date('Y-m-d',strtotime($row['reservation_date'])));
 				$reservation->addChild('GrossRevenue', $row['total_amount']);
 				$reservation->addChild('ReservationDate',date('Y-m-d H:i A',strtotime($row['created_date'])));
 				$reservation->addChild('AverageRoomRate', $row['average_room_rate']);
@@ -87,30 +93,30 @@ try {
 				$reservation->addChild('Name', $name);
 				$reservation->addChild('HotelID', $row['property_id']); 
 				$reservation->addChild('TotalNoOfRooms', $row['total_no_of_rooms']);
-				$reservation->addChild('BaseRatePlan', $row['base_rate']);
-
 
 			} else {
-
-				$Roomtype = $roomTypes->addChild('RoomType', $row['room_name']);
-				$Roomtype->addAttribute('ID', $row['id']);
+				
+				$ratePlan['totalRoomBaseRate'] += $row['base_rate'];
+				$Roomtype = $ratePlan->addChild('RoomType', $row['room_name']);
+				$Roomtype->addAttribute('ID', $row['room_id']);
 				$Roomtype->addAttribute('NoOfRooms', $row['no_of_rooms']);
 				$Roomtype->addAttribute('BaseRate', $row['base_rate']);
+				$Roomtype->addAttribute('Date',date('Y-m-d',strtotime($row['reservation_date'])));
 
 			}
 
 		}
 
-  	}
-	echo $xmlObj->asXML();
+  	} 
+
+  	echo $xmlObj->asXML();
+
 } catch(Exception $e) {
- var_export($e);
- 	// http_response_code(500);
+ 
+ 	http_response_code(500);
 
 }
 
-
-
-
-
 ?>
+
+
